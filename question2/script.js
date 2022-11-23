e298f05611eabdd48343fa21ebca2f526afe72ca
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2022-11-23 20:07:23 +0330

    question2

---
+++ question2/script.js
@@ -0,0 +1,13 @@
+const table = document.getElementById("age-table")
+console.log(table)
+const labels = table.querySelectorAll("label")
+console.log(labels)
+const td= table.querySelector("td")
+console.log(td)
+const form = document.forms.search
+console.log(form)
+const firstInput = form.querySelector("input")
+console.log(firstInput)
+const inputs = form.querySelectorAll("input")
+const lastInput=inputs[inputs.length-1]
+console.log(lastInput)
